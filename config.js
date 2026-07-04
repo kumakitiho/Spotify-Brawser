5eb90600020b0b0e223cfa44d93c056351263bba
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 03:31:48 +0900

    Bump reference layer after removing lower-left controls

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ function loadShelfModeTurntableSkin() {
             key: 'turntable-physics'
         },
         {
-            href: 'turntable-reference-match.css?v=20260705-reference-match-v2',
+            href: 'turntable-reference-match.css?v=20260705-reference-match-v3',
             key: 'turntable-reference-match'
         }
     ];
@@ -52,7 +52,7 @@ function loadShelfModeTurntableSkin() {
             key: 'turntable-polish'
         },
         {
-            src: 'turntable-reference-match.js?v=20260705-reference-match-v2',
+            src: 'turntable-reference-match.js?v=20260705-reference-match-v3',
             key: 'turntable-reference-match'
         }
     ];
