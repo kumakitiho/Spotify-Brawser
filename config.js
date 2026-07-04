8c9bf9b2d7dd4167b9a9e509f7246ae73fd81679
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 02:32:25 +0900

    Bump cue angle cache version

--- config.js
+++ config.js
@@ -29,14 +29,14 @@ function loadShelfModeTurntableSkin() {
             key: 'record-cover-boost'
         },
         {
-            href: 'turntable-physics.css?v=20260705-physics-v3',
+            href: 'turntable-physics.css?v=20260705-physics-v4',
             key: 'turntable-physics'
         }
     ];
 
     const scripts = [
         {
-            src: 'turntable-physics.js?v=20260705-physics-v3',
+            src: 'turntable-physics.js?v=20260705-physics-v4',
             key: 'turntable-physics'
         }
     ];
