3193e0a65fef93330894651c52f052c18afc4850
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 02:57:48 +0900

    Bump headshell and strobe cache versions

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ function loadShelfModeTurntableSkin() {
             key: 'record-cover-boost'
         },
         {
-            href: 'turntable-physics.css?v=20260705-physics-v6',
+            href: 'turntable-physics.css?v=20260705-physics-v7',
             key: 'turntable-physics'
         }
     ];
@@ -40,7 +40,7 @@ function loadShelfModeTurntableSkin() {
             key: 'turntable-physics'
         },
         {
-            src: 'turntable-arm-detail.js?v=20260705-arm-detail-v2',
+            src: 'turntable-arm-detail.js?v=20260705-arm-detail-v3',
             key: 'turntable-arm-detail'
         }
     ];
