a197c8abf0f4f2af1bba5300992b40b54657fe4c
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 05:11:48 +0900

    Bump reference cache after aligning platter dots

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ function loadShelfModeTurntableSkin() {
             key: 'turntable-physics'
         },
         {
-            href: 'turntable-reference-match.css?v=20260705-reference-match-v5',
+            href: 'turntable-reference-match.css?v=20260705-reference-match-v6',
             key: 'turntable-reference-match'
         }
     ];
@@ -52,7 +52,7 @@ function loadShelfModeTurntableSkin() {
             key: 'turntable-polish'
         },
         {
-            src: 'turntable-reference-match.js?v=20260705-reference-match-v5',
+            src: 'turntable-reference-match.js?v=20260705-reference-match-v6',
             key: 'turntable-reference-match'
         },
         {
