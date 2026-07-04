c88e73da150544d90752b7d537dc14f3069b19ba
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 04:14:31 +0900

    Bump physics cache for click replay fix

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ function loadShelfModeTurntableSkin() {
 
     const scripts = [
         {
-            src: 'turntable-physics.js?v=20260705-physics-v6',
+            src: 'turntable-physics.js?v=20260705-physics-v7',
             key: 'turntable-physics'
         },
         {
